360db312aba797aeb160281e02b56cbdb709bbf4
Dennis Jackson <git@dennis-jackson.uk>
2026-03-08 16:25:58 +0800

    Remove image tests

--- functions/nav/page1.js
+++
@@ -1,9 +0,0 @@
-export async function onRequest(context) {
-  const html = await context.env.ASSETS.fetch(new URL("/nav-page1.html", context.request.url));
-  const response = new Response(html.body, html);
-  response.headers.set(
-    "Integrity-Policy-WAICT-v1",
-    'manifest="/waict-manifest-page1.json", blocked-destinations=(image), mode=enforce, max-age=0'
-  );
-  return response;
-}
--- functions/nav/page2.js
+++
@@ -1,9 +0,0 @@
-export async function onRequest(context) {
-  const html = await context.env.ASSETS.fetch(new URL("/nav-page2.html", context.request.url));
-  const response = new Response(html.body, html);
-  response.headers.set(
-    "Integrity-Policy-WAICT-v1",
-    'manifest="/waict-manifest-page2.json", blocked-destinations=(image), mode=enforce, max-age=0'
-  );
-  return response;
-}
